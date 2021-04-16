cc3b1d0eead5a78da6cb388fd934611126664445
Betensis <Betensis@gmail.com>
2021-04-16 15:50:04 +0200

    hotfix

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import os
 import datetime
-from aiogram import Bot, Dispatcher, executor, types
 
 from config.config import TELEGRAM_TOKEN, MEDIA_DIR, LOG_DIR, LOGGING_LVL
 from validators import url_validator
 from youtube import download_yt_video
+
 from pytube import exceptions as yt_exceptions
 from asyncio import Lock
 from loguru import logger
+from aiogram import Bot, Dispatcher, executor, types
+import aiogram
 
 
 MEDIA_DIR.mkdir(exist_ok=True)
@@ -51,30 +53,42 @@ async def common_message(mes: types.Message):
             str(mes.text),
             f"media/{mes.chat.id}",
         )
-    except KeyError:
-        mes.answer('Видимо вы отправили ссылку на '
-                   'незаконченную трансляцию.... не надо так')
+    except KeyError as err:
+        await mes.answer(
+            "Видимо вы отправили ссылку на " + "трансляцию.... не надо так"
+        )
+        logger.info(f"Send stream link: |{mes.text=}|{err=}|{mes.chat.id=}")
+        return
     except yt_exceptions.VideoPrivate as err:
-        mes.answer("Видео находится в приватном доступе")
-        logger.info(err)
+        await mes.answer("Видео находится в приватном доступе")
+        logger.info(f"Send private video: |{mes.text=}|{err=}|{mes.chat.id=}")
         return
     except yt_exceptions.VideoRegionBlocked as err:
-        mes.answer("Видео заблочено в моем регионе")
-        logger.info(err)
+        await mes.answer("Видео заблочено в моем регионе")
+        logger.info(f"Blocked video: |{mes.text=}|{err=}|{mes.chat.id=}")
         return
     except yt_exceptions.VideoUnavailable as err:
-        mes.answer("Видео недоступно по хз какой причине")
-        logger.info(err)
+        await mes.answer("Видео недоступно по хз какой причине")
+        logger.info(f"Video Unavaible: |{mes.text=}|{err=}|{mes.chat.id=}")
         return
 
     await mes.answer("Видео скачано, отправляю...")
-    await mes.answer_audio(types.InputFile(file_path))
-    # delete audio file
+
     try:
-        os.remove(file_path)
-        logger.info(f"{file_path=} was deleted")
-    except OSError as err:
-        logger.error(err)
+        await mes.answer_audio(types.InputFile(file_path))
+    except aiogram.exceptions.NetworkError:
+        await mes.answer(
+            "Слишком длинное и тяжелое видео... " +
+            "телеграм не может такое отправить"
+        )
+        logger.info(f'Video too large: |{mes.text=}|{err=}|{mes.chat.id=}')
+    finally:
+        # delete audio file
+        try:
+            os.remove(file_path)
+            logger.info(f"{file_path=} was deleted")
+        except OSError as err:
+            logger.error(err)
 
 
 if __name__ == "__main__":
